99839edac9321e344c221f9198f51e5f0ad885b0
chanchalagnihotri87 <74895265+chanchalagnihotri87@users.noreply.github.com>
2025-10-25 12:25:11 +0530

    Update donorPortalUrl in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'https://vein360-api-dev-auekcfcdfkdggace.canadacentral-01.azurewebsites.net',
-  donorPortalUrl: 'https://gentle-glacier-01a07a80f.6.azurestaticapps.net',
+  donorPortalUrl: 'https://jolly-plant-0edcddf0f.3.azurestaticapps.net',
 };
